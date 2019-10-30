aa35e5f04595b5315a9f19c71cfe90deb2ed5939
Bruno Lehnen <bruno.lehnen@gmail.com>
2019-10-30 17:55:56 -0300

    Remove unnecessary function

--- src/server/render.ts
+++ src/server/render.ts
@@ -1,20 +1,20 @@
 import { Context } from 'koa';
 import * as path from 'path';
-import * as pug from 'pug';
+import { renderFile } from 'pug';
 
 const VIEW_DIR: string = 'views';
 const DEFAULT_TEMPLATE: string = 'default.pug';
 
-function baseRender(template: string, data: any) {
-    this.response.body = pug.renderFile(
-        path.join(__dirname, '..', VIEW_DIR, template),
-        Object.assign({}, this.state, data),
-    );
-}
-
 export async function render(ctx: Context, next: () => any) {
-    ctx.render = baseRender.bind(ctx);
-    ctx.renderDefault = baseRender.bind(ctx, DEFAULT_TEMPLATE);
+    ctx.render = (template: string, state: any, data: any) => {
+      return renderFile(
+          path.join(__dirname, '..', VIEW_DIR, template),
+          Object.assign({}, state, data),
+      );
+    };
+
+    ctx.renderDefault = (data: any) => ctx.render(DEFAULT_TEMPLATE, ctx.state, data);
 
     await next();
 }
+
